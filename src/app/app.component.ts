2b95126f2e1e424ac244050e49b340b096739a48
dongsu-iis <dongsu.workout@outlook.jp>
2020-01-15 00:53:45 +0900

    finished home work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,24 @@ export class AppComponent {
   title = 'demo1';
   url = 'http://blog.miniasp.com/';
   imgUrl = '/assets/images/logo.png';
+  searchTextCount = 0;
 
   constructor() {
-
   }
 
-  changeTitle() {
-    this.title = 'The Will Will Web';
+  changeTitle(altKey: boolean) {
+    if (altKey) {
+      this.title = 'The Will Will Web';
+    }
   }
+
+  getTextCount(searchText: string) {
+    this.searchTextCount = searchText.length;
+  }
+
+  inputReset(input: HTMLInputElement) {
+    input.value = '';
+    this.searchTextCount = 0;
+  }
+
 }
