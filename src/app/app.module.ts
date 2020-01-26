a190528a7b30ef679e26c802e69b4ebbfe69884c
dongsu-iis <dongsu.workout@outlook.jp>
2020-01-27 00:08:01 +0900

    Component Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormsModule } from '@angular/forms';
 
 import { Page1Component } from './page1/page1.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
