9c83dbdecfdf11fa441cb81c0bee8b3071c9c785
dongsu-iis <dongsu.workout@outlook.jp>
2020-01-31 00:04:40 +0900

    create featuer module

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleHeaderComponent } from './article-header/article-header.component';
+import { ArticleBodyComponent } from './article-body/article-body.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ArticleListComponent, ArticleHeaderComponent, ArticleBodyComponent],
+  exports: [ArticleListComponent],
   imports: [
     CommonModule
   ]
